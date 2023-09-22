eb5afa5de2099695f6d7de9c1168fde1f65e8d6e
Erçin Ural <ercin39@icloud.com>
2023-09-22 10:30:35 +0300

    sample

---
+++ js-sample/question.js
@@ -0,0 +1,32 @@
+
+
+let randomNumber = Math.floor(Math.random()*100);
+console.log("Random number " + randomNumber);
+let randomNumberfirst = Math.floor(randomNumber/10);
+console.log("Random First " + randomNumberfirst);
+let randomNumberSecond = randomNumber%10;
+console.log("Random Second " + randomNumberSecond);
+
+
+ function  tahminEt(prediction){
+    let predictionFirst = Math.floor(prediction / 10);
+    let predictionSeconde = prediction % 10;
+    if(randomNumber == prediction){
+        console.log("10000 TL Kazandınız!");
+    }else if(randomNumberfirst == predictionSeconde && 
+            randomNumberSecond == predictionFirst
+         ){
+            console.log("5000 TL Kazandınız.!");
+    }else if(
+        randomNumberfirst == predictionFirst ||
+        randomNumberfirst == predictionSeconde ||
+        randomNumberSecond == predictionFirst||
+        randomNumberSecond == predictionSeconde
+    ){
+        console.log("1000 tl kazandınız.");
+    }else{
+        console.log("Hiç bilemediniz.");
+    }
+}
+
+tahminEt(45);
